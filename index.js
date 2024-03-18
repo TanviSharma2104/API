14bdeda4b62f3df4ec8a26ac024481a8679dcd1e
TanviSharma2104 <tanvisharma2703@gmail.com>
2024-03-18 13:33:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors({ origin: "*", credentials: true }));
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/bfhl", (req, res) => {
+	try {
+		const user = "Tanvi_21042003";
+		const userEmail = "tanvi1440.be21@chitkara.edu.in";
+		const collegeRollNumber = "2110991440";
+
+		// Updated error handling for inputArray
+		const inputArray = req.body.data || [];
+		const evenNumbers = inputArray.filter((num) => !isNaN(num) && num % 2 === 0);
+		const oddNumbers = inputArray.filter((num) => !isNaN(num) && num % 2 !== 0);
+		const alphabetArray = inputArray
+			.filter((char) => /^[a-zA-Z]$/.test(char))
+			.map((char) => char.toUpperCase());
+
+		console.log(evenNumbers, inputArray);
+
+		const response = {
+			is_success: true,
+			user_id: user,
+			email: userEmail,
+			roll_number: collegeRollNumber,
+			odd_numbers: oddNumbers,
+			even_numbers: evenNumbers,
+			alphabets: alphabetArray,
+		};
+
+		res.status(200).json(response);
+	} catch (error) {
+		res.status(500).json({ error: error.message, is_success: false });
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
